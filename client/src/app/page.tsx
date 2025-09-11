73584f461f7a0657fb34262a650ffe0c04d5868c
sarathcredot <sarath@credot.in>
2025-09-11 20:49:16 +0530

    first setup

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-full h-screen bg-amber-700 text-white flex justify-center items-center" >
+
+      <h1 className="text-[30px]" > Pentasae.com </h1>
+
+    </div>
+  );
+}

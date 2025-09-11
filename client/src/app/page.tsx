29f8eb4cde40cc0cb2eb68606759fa90d514711e
sarathcredot <sarath@credot.in>
2025-09-11 22:16:38 +0530

    new added

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="w-full h-screen bg-amber-700 text-white flex justify-center items-center" >
 
-      <h1 className="text-[30px]" > Pentasae.com </h1>
+      <h1 className="text-[35px]" > Pentasae.com </h1>
 
     </div>
   );
